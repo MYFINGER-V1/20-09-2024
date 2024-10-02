cd8e252ddfecf1a402c63e993659f27f72d3c075
MYFINGER <julianalberto448@gmail.com>
2024-10-02 00:53:54 -0500

    v3

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     app: {
-        port: process.env.PORT || 8080,
+        port: process.env.PORT || 8096,
     },
     jwt: {
         secret: process.env.JWT_SECRET || 'notasecreta!'
